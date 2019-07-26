4b791ebbfd97db91d252d82eca26ff219fb47d63
Vasilijs <vasilijs.jelcaninovs@mogofinance.com>
2019-07-26 18:28:55 +0300

    controller to concern

---
+++ app/controllers/concerns/fileable.rb
@@ -0,0 +1,30 @@
+module Fileable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_file, only: [:delete_file]
+  end
+
+  def delete_file
+    binding.pry
+    @file.purge
+    
+    redirect_back(
+      fallback_location: documents_url,
+      otice: "File was successfuly deleted."
+    )
+  end
+
+  private
+
+    def set_file
+      @file = ActiveStorage::Attachment.find(params[:id])
+
+      return if AuthUtils.valid_user_for_file?(current_user, @file)
+
+      redirect_back(
+        fallback_location: documents_url,
+        notice: "You are not the owner of the file"
+      )
+    end
+end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,30 +1,23 @@
 class DocumentsController < ApplicationController
+  include Fileable
+
   before_action :set_document, only: [:show, :edit, :update, :destroy]
-  before_action :set_file, only: [:delete_file]
   before_action :check_user, only: [:new, :create]
 
-  # GET /documents
-  # GET /documents.json
   def index
     @documents = Document.all
   end
 
-  # GET /documents/1
-  # GET /documents/1.json
   def show
   end
 
-  # GET /documents/new
   def new
     @document = Document.new
   end
 
-  # GET /documents/1/edit
   def edit
   end
 
-  # POST /documents
-  # POST /documents.json
   def create
     @document = Document.new(document_params)
 
@@ -37,8 +30,6 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /documents/1
-  # PATCH/PUT /documents/1.json
   def update
     if @document.update(document_params)
       redirect_to @document, notice: 'Document was successfully updated.'
@@ -47,25 +38,13 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # DELETE /documents/1
-  # DELETE /documents/1.json
   def destroy
     @document.destroy
     
     redirect_to(documents_url, notice: "Document was successfully destroyed.")
   end
 
-  def delete_file
-    @file.purge
-    
-    redirect_back(
-      fallback_location: documents_url,
-      otice: "File was successfuly deleted."
-    )
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_document
       @document = Document.find(params[:id])
 
@@ -77,21 +56,10 @@ class DocumentsController < ApplicationController
       )
     end
 
-    def set_file
-      @file = ActiveStorage::Attachment.find(params[:id])
-
-      return if AuthUtils.valid_user_for_file?(current_user, @file)
-
-      redirect_back(
-        fallback_location: documents_url,
-        notice: "You are not the owner of the file"
-      )
-    end
-
     def check_user
       redirect_to(new_user_session_path) if current_user.blank?
     end
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def document_params
       params.fetch(:document, {}).permit(:name, :description, files: [])
     end
